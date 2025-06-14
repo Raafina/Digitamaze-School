4d681468d0167eb37e7b92eac509eb00d8b2e1ea
Raafina <raafinuradzani16@gmail.com>
2025-06-14 17:07:53 +0700

    Init: Laravel & Model-Migration

---
+++ app/Models/Teacher.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Teacher extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $protected = [
+        'id',
+    ];
+
+    protected $fillable = [
+        'nip',
+        'name',
+        'sex',
+        'phone',
+        'email',
+    ];
+}
